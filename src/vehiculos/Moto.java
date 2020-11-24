5acc94eb2a58e212ace4bb4096ec39c537ba5581
NicoCI <ibisshagrat@hotmail.com>
2020-11-24 12:53:56 +0100

    Codigo de Albaladejo

--- src/vehiculos/Moto.java
+++ src/vehiculos/Moto.java
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-package vehiculos;
 
-public class Moto extends VehiculoConRuedas {
-
-	public Moto () {
-		super();
-	}
-	
-	public Moto(String modelo, String color, String matricula) {
-		super(modelo, color, 2, matricula);
-	}
-
-	public Moto(String modelo, String color) {
-		super(modelo, color, 2);
-	}
-}
-=======
 package vehiculos;
 
 public class Moto extends VehiculoConRuedas {
@@ -33,6 +16,5 @@ public class Moto extends VehiculoConRuedas {
 	public String toString() {
 		return "Moto: " + super.toString();
 	}
-
 }
->>>>>>> 1fa568a36c5e8661f50af259ceb7c762f5acf50c
+
--- src/vehiculos/VehiculoConRuedas.java
+++ src/vehiculos/VehiculoConRuedas.java
@@ -1,46 +1,6 @@
 package vehiculos;
 
 public abstract class VehiculoConRuedas extends Vehiculo {
-<<<<<<< HEAD
-		
-		private String matricula;
-		private int cantidadRuedas;
-	
-		public String getMatricula() {
-			return matricula;
-			
-		}
-		
-		public int getNumeroDeRuedas() {
-			return cantidadRuedas;
-		}
-		
-		@Override
-		public String toString() {	//No se utiliza el toString de vehiculo por incluir dos veces el modelo, y la variable MODELO_NO_DISPONIBLE no es accesible por esta clase.
-			return (super.getModelo() == null ? "N/A" : super.getModelo())
-					+ " (" + super.getColor() + ")" 
-					+ " Matricula: " + matricula;
-		}
-		
-		public VehiculoConRuedas() {
-			this((String) null, (String) null, 0, (String) null);
-			}
-		
-		public VehiculoConRuedas(String modelo, String color) {
-			this(modelo, color, 0, (String) null);
-		}
-		
-		public VehiculoConRuedas(String modelo, String color, int cantidadRuedas) {
-			this(modelo, color, cantidadRuedas, (String) null);
-		}
-		
-		public VehiculoConRuedas(String modelo, String color, int cantidadRuedas, String matricula) {
-			super(modelo, color);
-			this.cantidadRuedas = cantidadRuedas;
-			this.matricula = matricula;
-		}
-}
-=======
 
 	public VehiculoConRuedas() {
 		super();
@@ -56,6 +16,5 @@ public abstract class VehiculoConRuedas extends Vehiculo {
 	public String toString() {
 		return super.toString() + ", " + getNumeroDeRuedas() + " ruedas";
 	}
-
 }
->>>>>>> 1fa568a36c5e8661f50af259ceb7c762f5acf50c
+
